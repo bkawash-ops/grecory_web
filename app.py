60d7d6f22d22509bf731676dc9e198ae16e7a6a0
bkawash-ops <bkawash@gmail.com>
2026-07-15 13:58:36 +0300

    Update app.py

--- app.py
+++ app.py
@@ -224,6 +224,39 @@ def test_customer(id):
     conn.close()
 
     return str(customer)
+
+@app.route("/fix_customer_codes")
+def fix_customer_codes():
+
+    conn = db()
+    cur = conn.cursor()
+
+    cur.execute("""
+        SELECT id
+        FROM customers
+        WHERE customer_code IS NULL
+        ORDER BY id
+    """)
+
+    customers = cur.fetchall()
+
+    for row in customers:
+        cid = row[0]
+
+        code = f"CUS-{cid:05d}"
+
+        cur.execute("""
+            UPDATE customers
+            SET customer_code=%s
+            WHERE id=%s
+        """,(code,cid))
+
+    conn.commit()
+
+    cur.close()
+    conn.close()
+
+    return "DONE"
 @app.route("/customer/<int:id>")
 def customer_account(id):
 
